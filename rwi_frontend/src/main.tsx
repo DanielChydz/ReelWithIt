82a0ba4863232945a1ce94cc61fdd1ea57590d40
DanielChydz <daniel.chydz@gmail.com>
2025-08-01 20:03:31 +0200

    Frontend auth work in progress

--- rwi_frontend/src/main.tsx
+++ rwi_frontend/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css'
-import HomePage from './App.tsx'
+import HomePage from './pages/HomePage.tsx'
+import LoginPage from './pages/LoginPage.tsx';
+import RegisterPage from './pages/RegisterPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/auth/login" element={<LoginPage/>} />
+        <Route path="/auth/register" element={<RegisterPage/>} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
